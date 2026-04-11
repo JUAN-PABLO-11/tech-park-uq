f73171d2fcfea8d23a8d0fd1d3772eb4e035ac28
juan pablo <juanp.espinosap@uqvirtual.edu.co>
2026-04-10 21:42:49 -0500

    feat: arbol y busqueda binaria

---
+++ project_code/NodoArbol.java
@@ -0,0 +1,14 @@
+package project_code;
+
+public class NodoArbol<T> {
+    public T dato;
+    public NodoArbol<T> izquierdo;
+    public NodoArbol<T> derecho;
+
+    public NodoArbol(T dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+}
