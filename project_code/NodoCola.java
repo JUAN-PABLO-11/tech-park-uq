9e5ea4583951de54e35576a5600441ea8e362c8a
juan pablo <juanp.espinosap@uqvirtual.edu.co>
2026-04-10 21:06:18 -0500

    feat: cola de prioridad

---
+++ project_code/NodoCola.java
@@ -0,0 +1,13 @@
+package project_code;
+
+public class NodoCola<T> {
+    public T dato;
+    public int prioridad;
+    public NodoCola<T> siguiente;
+
+    public NodoCola(T dato, int prioridad) {
+        this.dato = dato;
+        this.prioridad = prioridad;
+        this.siguiente = null;
+    }
+}
